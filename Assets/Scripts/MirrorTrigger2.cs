a61f05806864a223e213c2edcee364441a49cbe5
Abhinav Nair <53712831+nav800@users.noreply.github.com>
2024-10-27 03:16:16 -0700

    Tweaks, corridor 2 and room 3

--- Assets/Scripts/MirrorTrigger2.cs
+++ Assets/Scripts/MirrorTrigger2.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MirrorTrigger : MonoBehaviour
+public class MirrorTrigger2 : MonoBehaviour
 {
     public GameObject Target;
     private Collider2D TargetCollider;
+    private SpriteRenderer TargetSprenderer;
 
     void Start(){
         Target.TryGetComponent<Collider2D>(out TargetCollider);
+        Target.TryGetComponent<SpriteRenderer>(out TargetSprenderer);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -15,12 +17,13 @@ public class MirrorTrigger : MonoBehaviour
         Debug.Log(other.tag);
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Activate the dialogue and the goal");
+            Debug.Log("Activate the dialogue and reveal weapon");
 
             // Enable the collider
-            if (TargetCollider != null)
+            if (TargetCollider != null && TargetSprenderer != null)
             {
                 TargetCollider.enabled = true;
+                TargetSprenderer.enabled = true;
             }
         }
     }
